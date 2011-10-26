b8a333d272a04d0a377ced8d8fe885f1a860b870
Patryk Dwórznik <dwuziu@bzium.net>
2011-10-26 23:56:07 +0200

    poker-eval source removed

--- poker-eval/extconf.rb
+++
@@ -1,9 +0,0 @@
-# Loads mkmf which is used to make makefiles for Ruby extensions
-require 'mkmf'
-# Give it a name
-extension_name = 'extpokereval'
-
-# The destination
-dir_config(extension_name)
-
-puts `CC='gcc -fPIC' ./configure --prefix=#{Dir.pwd}/../../lib/poker_eval`
